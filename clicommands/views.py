2f9446685d5103f6e8db56a0c3761eded2e678f2
GoldenbyteGH <obertigiovannicesare@gmai.com>
2022-12-30 23:39:21 +0100

    publish directly if staff

--- clicommands/views.py
+++ clicommands/views.py
@@ -8,6 +8,8 @@ def add(request):
         form = DefineCommandForm(request.POST)
         if form.is_valid():
             form.instance.owner = request.user
+            if request.user.is_staff:
+                form.instance.is_published = True
             form.save()
     return redirect('dashboard')
 
@@ -16,7 +18,10 @@ def edit(request,command_id):
         command = get_object_or_404(Command, pk=command_id)
         form = DefineCommandForm(instance=command,data=request.POST)
         if form.is_valid():
-            form.instance.is_published = False          # wait 4 review to publish again
+            if request.user.is_staff:
+                form.instance.is_published = True
+            else:
+                form.instance.is_published = False       # wait 4 review to publish again if not staff
             form.save()
     return redirect('dashboard')
 
