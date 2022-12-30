c76636e870c3e69da3e64204c31b2d5e2247ae93
GoldenbyteGH <obertigiovannicesare@gmai.com>
2022-12-30 11:04:48 +0100

    check_4_email_duplicate2

--- accounts/forms.py
+++ accounts/forms.py
@@ -19,16 +19,14 @@ user_model = get_user_model()
 #     captcha = CaptchaField()
 
 class SignUpForm(UserCreationForm):
-
-
-    email = forms.EmailField(
-        max_length=254, help_text='Enter a valid email address')
-
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('That email is being used')
         return email
+
+    email = forms.EmailField(
+        max_length=254, help_text='Enter a valid email address')
 
     class Meta:
         model = user_model
